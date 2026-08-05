74a7490c027a2c987ca1286e34c77d00eef30b29
kasim-7977 <mohammedkasim7977@gmail.com>
2026-08-05 22:24:25 +0530

    Update FoodCard.jsx

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,33 +1,55 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
-import './FoodCard.css';
 
 function FoodCard({ food }) {
   const { addToCart } = useContext(CartContext);
+
   return (
-    <div className="col-md-4 mb-4">
-      <div className="comedy">
-      <img
-  src={food.image}
-  alt={food.name}
-  className="card-img-top"
-  style={{ height: "200px", objectFit: "cover" }}
-/>
+    <div className="col-lg-4 col-md-6">
+      <div
+        className="card border-0 shadow h-100"
+        style={{
+          borderRadius: "20px",
+          overflow: "hidden",
+          transition: ".3s",
+        }}
+      >
+        <img
+          src={food.image}
+          alt={food.name}
+          style={{
+            height: "230px",
+            objectFit: "cover",
+          }}
+        />
 
-      <div className="card-body">
-        <h5>{food.name}</h5>
+        <div className="card-body">
 
-        <p>{food.category_name}</p>
+          <h4 className="fw-bold">
+            {food.name}
+          </h4>
 
-        <h6>₹{food.price}</h6>
-<button
-  className="btn btn-primary"
-  onClick={() => addToCart(food)}
->
-  Add To Cart
-</button>
+          <p className="text-muted">
+            {food.description}
+          </p>
+
+          <div className="d-flex justify-content-between align-items-center">
+
+            <h4 className="text-danger">
+              ₹{food.price}
+            </h4>
+
+            <button
+              className="btn btn-warning rounded-pill px-4"
+              onClick={() => addToCart(food)}
+            >
+              Add
+            </button>
+
+          </div>
+
+        </div>
       </div>
-      </div>  
     </div>
   );
 }
