9f85e584bd7fcec5518b2248fefbeb098ca44ed4
kasim-7977 <mohammedkasim7977@gmail.com>
2026-08-05 17:28:56 +0530

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import "./Home.css"; 
+import Hero from "../components/Hero";
+import CategorySection from "../components/CategorySection";
+import PopularFoods from "../components/PopularFoods";
+import Features from "../components/Features";
+import DownloadApp from "../components/DownloadApp";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
-    // Style the wrapper div to cover the whole screen
-    <div className="container mt-5 dd" style={{ backgroundColor: 'burlywood', minHeight: '100vh' }}>
-    <h1>W<span>e</span>l<span>c</span>o<span>m</span>e T<span>o</span> M<span>y</span>   🐼   F<span>o</span>o<span>d</span> D<span>e</span>l<span>i</span>v<span>e</span>r<span>y</span> A<span>p</span>p</h1>
-    <p class="lead text-start mt-4">Welcome to FoodExpress! Discover delicious meals from your favorite restaurants and get them delivered to your doorstep quickly and conveniently.</p>
-    </div>  
+    <>
+      <Hero />
+      <CategorySection />
+      <PopularFoods />
+      <Features />
+      <DownloadApp />
+      <Footer />
+    </>
   );
 }
 
