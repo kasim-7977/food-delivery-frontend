58885b1415f40616bc4d6a01d9c1c637fc449892
kasim-7977 <mohammedkasim7977@gmail.com>
2026-08-06 11:42:57 +0530

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,10 @@
+import './Portfolio.css';
 function Portfolio(){
     return(
         // body
         <div className="container">
         <h1 className=" col-6 mt-4 text-primary">MOHAMMED KASIM</h1>
-        <img className="col-6 " src="https://res.cloudinary.com/qgbhmceb/image/upload/v1785996413/kasim_profile_img_rounded_l8xw9v.png height=100px width=100px" alt="MyImg" />
+        <img className="col-6 " src="https://res.cloudinary.com/qgbhmceb/image/upload/v1785996413/kasim_profile_img_rounded_l8xw9v.png " alt="MyImg" />
         </div>
         )
     }
